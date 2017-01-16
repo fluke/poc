600f907d81b0388d7ef2ac07eff3590863d2be64
Kartik Luke Singh <kartikluke@gmail.com>
2017-01-16 10:45:07 +0530

    remove the wildcard from domain not getting recognized as the same cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store, key: '_justcode_session_id', domain: '.ec2-54-86-9-189.compute-1.amazonaws.com'
+Rails.application.config.session_store :active_record_store, key: '_justcode_session_id', domain: 'ec2-54-86-9-189.compute-1.amazonaws.com'
 
